b4cc59e3830ff732ffc8d6839b496d2d30891e6e
Debdutta Panda <bedmad82@gmail.com>
2022-03-31 23:49:28 +0530

    test_case_2

--- app/src/main/java/com/debduttapanda/androidunittestingsample/Validator.kt
+++ app/src/main/java/com/debduttapanda/androidunittestingsample/Validator.kt
@@ -1,7 +1,7 @@
 package com.debduttapanda.androidunittestingsample
 
 object Validator {
-    fun validateInput(amount: Int, desc: String){
-
+    fun validateInput(amount: Int, desc: String): Boolean{
+        return amount > 0 && desc.isNotEmpty()
     }
 }
